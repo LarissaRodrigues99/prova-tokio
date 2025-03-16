c2f87a80f857121932dcd55178939f40a5296c47
Larissa Rodrigues <larissarodrigues1599@gmail.com>
2025-03-16 15:35:52 -0300

    Fix: Criando classe de dominio

--- prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/entity/Addresses.java
+++ prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/entity/Addresses.java
@@ -1,9 +1,13 @@
-package br.com.tokiomarine.seguradora.domain;
+package br.com.tokiomarine.seguradora.entity;
 
 import lombok.*;
-import org.springframework.data.annotation.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.time.Instant;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Table(name="addresses")
 @Entity
@@ -13,18 +17,38 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Addresses {
-        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Id
+        @Column(name = "id")
         private Long id;
 
         @ManyToOne
-        @JoinColumn(name = "id_client", referencedColumnName = "id", nullable = false)
+        @JoinColumn(name = "id_client")
         private Clients client;
 
+        @Column(name = "address")
         private String address;
+
+        @Column(name = "number")
         private String number;
+
+        @Column(name = "complement")
         private String complement;
+
+        @Column(name = "postal_code")
         private String postal_code;
+
+        @Column(name = "city")
         private String city;
+
+        @Column(name = "state")
         private String state;
+
+        @Column(name = "country")
         private String country;
+
+        @CreationTimestamp
+        private Instant creationTimestamp;
+
+        @UpdateTimestamp
+        private Instant updateTimestamp;
 }
