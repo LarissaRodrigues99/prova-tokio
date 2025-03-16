1efa3becb4113006860dd6b11bed07d9bbe1dd3f
Larissa Rodrigues <larissarodrigues1599@gmail.com>
2025-03-16 15:36:47 -0300

    Fix: Criando classe dominio

--- prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/entity/Clients.java
+++ prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/entity/Clients.java
@@ -1,10 +1,15 @@
-package br.com.tokiomarine.seguradora.domain;
+package br.com.tokiomarine.seguradora.entity;
 
 
 import lombok.*;
-import org.springframework.data.annotation.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.time.Instant;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Table(name="clients")
 @Entity
@@ -14,14 +19,26 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Clients {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "first_name")
     private String first_name;
+
+    @Column(name = "last_name")
     private String last_name;
+
+    @Column(name = "email")
     private String email;
 
-//    @OneToMany(mappedBy = "client")
-//    private List<Addresses> addresses;
+    @OneToMany(mappedBy = "client")
+    private List<Addresses> addresses;
+
+    @CreationTimestamp
+    private Instant creationTimestamp;
+
+    @UpdateTimestamp
+    private Instant updateTimestamp;
 
 }
