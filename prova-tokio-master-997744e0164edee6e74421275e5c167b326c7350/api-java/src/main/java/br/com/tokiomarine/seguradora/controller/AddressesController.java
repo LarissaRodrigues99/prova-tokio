e78bd4303cac43ff1990d555c2c41c36c63c1477
Larissa Rodrigues <larissarodrigues1599@gmail.com>
2025-03-16 22:35:19 -0300

    Criando CRUD

--- prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/controller/AddressesController.java
+++ prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/controller/AddressesController.java
@@ -1,4 +1,43 @@
 package br.com.tokiomarine.seguradora.controller;
 
+import br.com.tokiomarine.seguradora.entity.Addresses;
+import br.com.tokiomarine.seguradora.repository.AddressesRepository;
+
+import br.com.tokiomarine.seguradora.service.AddressesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/v1/addresses")
 public class AddressesController {
+
+    @Autowired
+    private AddressesRepository repository;
+
+    @Autowired
+    private AddressesService addressesService;
+
+    @GetMapping
+    public ResponseEntity gerAlladdresses(){
+        var allUsers = repository.findAll();
+        return ResponseEntity.ok(allUsers);
+    }
+
+    @PostMapping
+    public ResponseEntity<Addresses> createAddresses(@RequestBody Addresses addresses) {
+        Addresses savedAddresses = addressesService.createAddresses(addresses);
+        return new ResponseEntity<>(savedAddresses, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletById(@PathVariable("id") Long id){
+
+        addressesService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/controller/ClientsController.java
+++ prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/controller/ClientsController.java
@@ -1,14 +1,39 @@
 package br.com.tokiomarine.seguradora.controller;
 
+import br.com.tokiomarine.seguradora.entity.Clients;
+import br.com.tokiomarine.seguradora.repository.ClientsRepository;
+import br.com.tokiomarine.seguradora.service.ClientsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/v1/users")
 public class ClientsController {
 
+    @Autowired
+    private ClientsRepository repository;
+
+    @Autowired
+    private ClientsService clientsService;
+
     @GetMapping
-    public ResponseEntity getAllClients(){
-        return ResponseEntity.ok("Helo World");
+    public ResponseEntity gerAllUsers(){
+        var allUsers = repository.findAll();
+        return ResponseEntity.ok(allUsers);
+    }
+
+    @PostMapping
+    public ResponseEntity<Clients> createClient(@RequestBody Clients clients) {
+        Clients savedClients = clientsService.createClients(clients);
+        return new ResponseEntity<>(savedClients, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletById(@PathVariable("id") Long id){
+
+        clientsService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
