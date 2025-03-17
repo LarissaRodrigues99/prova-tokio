83ee53691f839b55e22108940f536ebc2d132479
Larissa Rodrigues <larissarodrigues1599@gmail.com>
2025-03-16 22:33:02 -0300

    Adicionando anotação

--- prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/repository/AddressesRepository.java
+++ prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/repository/AddressesRepository.java
@@ -2,5 +2,7 @@ package br.com.tokiomarine.seguradora.repository;
 
 import br.com.tokiomarine.seguradora.entity.Addresses;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressesRepository extends JpaRepository<Addresses, Long> {}
--- prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/repository/ClientsRepository.java
+++ prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/repository/ClientsRepository.java
@@ -2,5 +2,7 @@ package br.com.tokiomarine.seguradora.repository;
 
 import br.com.tokiomarine.seguradora.entity.Clients;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientsRepository extends JpaRepository<Clients, Long> {}
