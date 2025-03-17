c96f161d562b98f51ba67e591e43cf2a89af34f2
Larissa Rodrigues <larissarodrigues1599@gmail.com>
2025-03-16 22:43:34 -0300

    Criando servise para que a aplicação consiga se comunicar com o banco de dados

--- prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/service/AddressesService.java
+++ prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/service/AddressesService.java
@@ -6,11 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AnddressesService {
+public class AddressesService {
     @Autowired
     private AddressesRepository addressesRepository;
 
     public Addresses createAddresses(Addresses addresses) {
         return addressesRepository.save(addresses);
+    }
+
+    public Addresses saveAddresses(Addresses addresses) {
+        return addressesRepository.save(addresses);
+    }
+
+    public void deleteById(Long id) {
+
+        var addressesExists = addressesRepository.existsById(id);
+
+        if (addressesExists) {
+            addressesRepository.deleteById(id);
+        }
     }
 }
--- prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/service/ClientsService.java
+++ prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/service/ClientsService.java
@@ -1,4 +1,28 @@
 package br.com.tokiomarine.seguradora.service;
 
+import br.com.tokiomarine.seguradora.entity.Clients;
+import br.com.tokiomarine.seguradora.repository.ClientsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class ClientsService {
+    @Autowired
+    private ClientsRepository clientsRepository;
+
+    public Clients createClients(Clients clients) {
+        return clientsRepository.save(clients);
+    }
+
+    public void deleteById(Long id) {
+
+        var userExists = clientsRepository.existsById(id);
+
+        if (userExists) {
+            clientsRepository.deleteById(id);
+        }
+    }
+
 }
