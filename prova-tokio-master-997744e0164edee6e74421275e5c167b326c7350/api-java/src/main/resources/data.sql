2c0d299506f2c849cc341d45a8901a3bfd4d6703
Larissa Rodrigues <larissarodrigues1599@gmail.com>
2025-03-18 17:57:20 -0300

    Adicionando comando para o banco de dados

--- prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/resources/data.sql
+++ prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/resources/data.sql
@@ -1,12 +1,12 @@
 
-CREATE TABLE IF NOT EXISTS clients (
+CREATE TABLE clients (
 	id INT NOT NULL PRIMARY KEY,
 	first_name VARCHAR(250) NOT NULL,
 	last_name VARCHAR(250),
 	email VARCHAR(250) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS addresses (
+CREATE TABLE addresses (
 	id INT NOT NULL PRIMARY KEY,
 	id_client INT NOT NULL,
 	address VARCHAR(250) NOT NULL,
@@ -19,3 +19,10 @@ CREATE TABLE IF NOT EXISTS addresses (
 	FOREIGN KEY (id_client) REFERENCES public.clients(id)
 );
 
+-- Dados iniciais para a tabela de clientes
+INSERT INTO clients (id, first_name, last_name, email) VALUES (1, 'Ana', 'Solza', 'ana@exemplo.com');
+INSERT INTO clients (id, first_name, last_name, email) VALUES (2, 'Junior', 'Sézar', 'junior@exemplo.com');
+
+-- Dados iniciais para a tabela de endereços
+INSERT INTO addresses (id, id_client, address, number, complement, postal_code, city, state, country)
+VALUES (1, 1, 'Rua Exemplo', '123', 'apto 1', '01001000', 'São Paulo', 'SP', 'Brasil');
